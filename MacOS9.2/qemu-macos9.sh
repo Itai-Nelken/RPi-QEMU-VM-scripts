3f8f29a24d0a4f37f0209455c948c1d62da97381
Itai Nelken <70802936+Itai-Nelken@users.noreply.github.com>
2021-01-24 22:00:33 +0200

    Update qemu-macos9.sh

--- MacOS9.2/qemu-macos9.sh
+++ MacOS9.2/qemu-macos9.sh
@@ -12,9 +12,9 @@ clear
 echo "this script will install qemu 5.2 and create a MacOS 9.2 VM for you."
 read -p "Do you want to proceed (y/n)?" choice
 case "$choice" in 
-  y|Y ) echo -e "$(tput setaf 2)$(tput bold)LOADING...$(tput sgr 0)";;
-  n|N ) echo "exiting..."; sleep 1; exit;;
-  * ) echo "invalid";;
+  y|Y ) echo -e "$(tput setaf 2)$(tput bold)LOADING...$(tput sgr 0)" ;;
+  n|N ) echo "exiting..."; sleep 1; exit ;;
+  * ) echo "invalid" ;;
 esac
 
 #loading bar
@@ -35,13 +35,21 @@ echo -ne '(0%)[##########################](100%)\r'
 sleep 0.5
 echo -ne '\n'
 
-#variables
-DIRECTORY="$HOME/Downloads"
-#CORES="`nproc`"
-DISKSIZE=2
 
-#clear the screen again
-clear
+#determine if host system is 64 bit arm64 or 32 bit armhf
+if [ ! -z "$(file "$(readlink -f "/sbin/init")" | grep 64)" ];then
+  echo -e "$(tput setaf 1)$(tput bold)This script can't run because your OS is 64bit!$(tput sgr 0)"
+  exit
+elif [ ! -z "$(file "$(readlink -f "/sbin/init")" | grep 32)" ];then
+  echo "OS is 32bit..."
+else
+  echo -e "$(tput setaf 1)$(tput bold)Can't detect OS architecture! something is very wrong!$(tput sgr 0)"
+  exit
+fi
+
+#variables
+#DISKSIZE=2
+
 
 #enter home folder
 cd $HOME
@@ -51,9 +59,7 @@ echo -e "$(tput setaf 3)Installing dependencies...$(tput sgr 0)"
 if ! which aria2c > /dev/null; then
    sudo apt install -y aria2
    clear
-elif ! which openbios-ppc > /dev/null; then
-  sudo apt install -y openbios-ppc
-  clear
+   aria2=1
 else
     echo "dependencies already installed..."
 fi
@@ -98,6 +104,11 @@ Path=$HOME/macos921
 Terminal=false
 StartupNotify=true" > ~/Desktop/macos9.desktop
 sudo chmod +x ~/Desktop/macos9.desktop
+
+if [[ "$aria2" == 1 ]]; then
+  echo "installed" > ~/macos921/aria2-installed
+fi
+
 echo -e "$(tput setaf 3)removing uneeded files...$(tput sgr 0)"
 rm ~/macos921.tar.xz
 rm ~/qemu_5.2_armhf.deb
